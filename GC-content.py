9e420c7be4b93d08562215cd2739f91aefa06027
LukaszReszka <01143598@pw.edu.pl>
2023-11-18 23:15:00 +0100

    Calculate GC-content

---
+++ GC-content.py
@@ -0,0 +1,19 @@
+from Bio.SeqIO import parse
+from Bio.SeqUtils import gc_fraction as GC
+from argparse import ArgumentTypeError, ArgumentParser as ArgP
+from os.path import isfile
+
+def file_path(arg_str):
+    if isfile(arg_str):
+        return arg_str
+    else:
+        raise ArgumentTypeError('{} is not a valid path to FASTA file!'.format(arg_str))
+
+arg_parser = ArgP(description='Calculates GC-content for sequences from provided FASTA file.')
+arg_parser.add_argument('pathToFASTA', type=file_path)
+args = arg_parser.parse_args()
+
+for sequence_record in parse(args.pathToFASTA, 'fasta'):
+    print('Sequence ID: {}'.format(sequence_record.id))
+    print('Sequence: {}'.format(repr(sequence_record.seq)))
+    print('GC-content: {} %'.format(100 * GC(sequence_record.seq))
